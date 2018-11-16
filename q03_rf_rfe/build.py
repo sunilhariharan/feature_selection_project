91e421c0bc2e90a0659044022ca667c943624de1
sunilhariharan <sunilhariharan@gmail.com>
2018-11-16 13:57:23 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -1,6 +1,7 @@
+# %load q03_rf_rfe/build.py
 # Default imports
 import pandas as pd
-
+import numpy as np
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 from sklearn.feature_selection import RFE
@@ -8,4 +9,15 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # Your solution code here
+def rf_rfe(df):
+    X=data.iloc[:,:-1]
+    y=data.iloc[:,-1]
+    model=RandomForestClassifier()
+    rfe = RFE(model, 17)
+    rfe = rfe.fit(X, y)
+    r=rfe.ranking_
+    l=np.where(r==1)[0].tolist()
+    top_features=X.iloc[:,l].columns.values.tolist()
+    return top_features
+
 
